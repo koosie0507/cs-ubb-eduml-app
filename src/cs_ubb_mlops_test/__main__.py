58af7c2754f44ee3fac3d833dc498f56ed0fe7be
Andrei Olar <andrei.olar@gmail.com>
2024-11-04 15:33:36 +0200

    mlflow artifact storage

--- src/cs_ubb_mlops_test/__main__.py
+++ src/cs_ubb_mlops_test/__main__.py
@@ -14,13 +14,20 @@ from sklearn.model_selection import train_test_split
 
 import mlflow.sklearn
 
+from cs_ubb_mlops_test.mlflow import sklearn_model
+
 dotenv.load_dotenv()
+warnings.filterwarnings("ignore")
+np.random.seed(40)
+
 
 MLFLOW_TRACKING_ENABLED = os.getenv("MLFLOW_TRACKING_ENABLED", "") in {
     "t", "true", "1", "yes", "y"
 }
 MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
-MINIO_ENABLED = os.getenv("MINIO_ENABLED", False)
+MINIO_ENABLED = os.getenv("MINIO_ENABLED", "") in {
+    "t", "true", "1", "yes", "y"
+}
 ROOT_DIR = Path(__file__).parent.parent.parent
 TRAINING_DATA_PATH = (
     os.getenv("MINIO_OBJECT_PATH", "cs-ubb-mlops-test-1/wine-quality.csv")
@@ -46,7 +53,8 @@ def load_data() -> pd.DataFrame:
     else:
         local_path = ROOT_DIR / TRAINING_DATA_PATH
         with open(local_path, "r"):
-            return pd.read_csv(local_path)
+            result = pd.read_csv(local_path)
+        return result
 
 
 def eval_metrics(actual, pred):
@@ -56,49 +64,37 @@ def eval_metrics(actual, pred):
     return rmse, mae, r2
 
 
-warnings.filterwarnings("ignore")
-np.random.seed(40)
+@sklearn_model(MLFLOW_TRACKING_ENABLED, MLFLOW_TRACKING_URI, "test-experiment-1")
+def fit_predict_wine_quality(a: float, l1: float):
+    wine_quality_df = load_data()
+    train, test = train_test_split(wine_quality_df)
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--alpha")
-parser.add_argument("--l1-ratio")
-args = parser.parse_args()
-alpha = float(args.alpha)
-l1_ratio = float(args.l1_ratio)
+    train_x = train.drop(["quality"], axis=1)
+    test_x = test.drop(["quality"], axis=1)
+    train_y = train[["quality"]]
+    test_y = test[["quality"]]
 
-print("parsed args alpha", alpha, "and l1 ratio", l1_ratio)
+    lr = ElasticNet(alpha=a, l1_ratio=l1, random_state=42)
+    lr.fit(train_x, train_y)
 
-wine_quality_df = load_data()
+    predicted_qualities = lr.predict(test_x)
+    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+    mlflow.log_metric("rmse", rmse)
+    mlflow.log_metric("r2", r2)
+    mlflow.log_metric("mae", mae)
+    return test_x, predicted_qualities, lr
+
 
 # Split the data into training and test sets. (0.75, 0.25) split.
-train, test = train_test_split(wine_quality_df)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--alpha")
+    parser.add_argument("--l1-ratio")
+    args = parser.parse_args()
+    alpha = float(args.alpha)
+    l1_ratio = float(args.l1_ratio)
+    print("parsed args alpha", alpha, "and l1 ratio", l1_ratio)
 
-# The predicted column is "quality" which is a scalar from [3, 9]
-train_x = train.drop(["quality"], axis=1)
-test_x = test.drop(["quality"], axis=1)
-train_y = train[["quality"]]
-test_y = test[["quality"]]
-
-if MLFLOW_TRACKING_ENABLED:
-    mlflow.set_tracking_uri(uri=MLFLOW_TRACKING_URI)
-
-for idx, alpha_counter in enumerate(range(2, int(alpha*10))):
-    ialpha = alpha_counter / 10
-    logfile_name = f"train-log-{idx+1}.log"
-    with mlflow.start_run(run_name=f"cs_ubb_mlops_test-{idx+1}") as run:
-        mlflow.log_text("start training", logfile_name, run.info.run_id)
-        lr = ElasticNet(alpha=ialpha, l1_ratio=l1_ratio, random_state=42)
-        lr.fit(train_x, train_y)
-        mlflow.log_text("done training", logfile_name, run.info.run_id)
-
-        predicted_qualities = lr.predict(test_x)
-
-        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
-
-        mlflow.log_param("alpha", ialpha)
-        mlflow.log_param("l1_ratio", l1_ratio)
-        mlflow.log_metric("rmse", rmse)
-        mlflow.log_metric("r2", r2)
-        mlflow.log_metric("mae", mae)
-        mlflow.log_text("done eval", logfile_name, run.info.run_id)
-        mlflow.sklearn.log_model(lr, "model")
+    for idx, alpha_counter in enumerate(range(2, int(alpha*10))):
+        ialpha = alpha_counter / 10
+        fit_predict_wine_quality(ialpha, l1_ratio)
---
+++ src/cs_ubb_mlops_test/mlflow/_decorator.py
@@ -0,0 +1,50 @@
+import functools
+from typing import Callable, Any, Optional
+
+import mlflow.sklearn
+from mlflow.entities import RunStatus
+from mlflow.models import infer_signature, ModelSignature
+
+
+class sklearn_model:
+    def __init__(self, enabled: bool, tracking_uri: str, experiment: Optional[str] = None) -> None:
+        if enabled:
+            mlflow.set_tracking_uri(tracking_uri)
+        self._experiment = experiment
+
+    def __ensure_experiment_id(self):
+        if not self._experiment:
+            return None
+        exp = mlflow.get_experiment_by_name(self._experiment)
+        if exp:
+            return exp.experiment_id
+        return mlflow.create_experiment(self._experiment)
+
+    def __call__(self, wrapped: Callable[..., ModelSignature]) -> Callable:
+        @functools.wraps(wrapped)
+        def wrapper(*args, **kwargs) -> Any:
+            status = "FINISHED"
+            try:
+                run_args = {}
+                experiment_id = self.__ensure_experiment_id()
+                if experiment_id:
+                    run_args["experiment_id"] = experiment_id
+
+                mlflow.start_run(**run_args)
+                model_params = {
+                    f"param_{idx+1}": arg
+                    for idx, arg in enumerate(args)
+                }
+                model_params.update(**kwargs)
+                mlflow.log_params(model_params)
+                data_in, model_out, model = wrapped(*args, **kwargs)
+                sig = infer_signature(data_in, model_out, model_params)
+                mlflow.sklearn.log_model(model, artifact_path="sklearn-model", signature=sig)
+                return model
+            except Exception as exc:
+                status = "FAILED"
+                raise exc
+            finally:
+                mlflow.end_run(status)
+
+        return wrapper
