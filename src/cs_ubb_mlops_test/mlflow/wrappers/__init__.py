0c66211bfa8276d172f52dca9b20674239d1ed4a
Andrei Olar <andrei.olar@gmail.com>
2024-11-07 13:26:21 +0200

    expose torch_model decorator

--- src/cs_ubb_mlops_test/mlflow/wrappers/__init__.py
+++ src/cs_ubb_mlops_test/mlflow/wrappers/__init__.py
@@ -1,4 +1,5 @@
 from cs_ubb_mlops_test.mlflow.wrappers._sklearn import sklearn_model
+from cs_ubb_mlops_test.mlflow.wrappers._torch import torch_model
 
 
-__all__ = ["sklearn_model"]
+__all__ = ["sklearn_model", "torch_model"]
