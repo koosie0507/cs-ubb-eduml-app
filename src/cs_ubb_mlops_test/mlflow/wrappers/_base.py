41172f646c8b694ef1f76d0b2a9962bb15ec4420
Andrei Olar <andrei.olar@gmail.com>
2024-11-07 13:25:23 +0200

    improve separation of concerns in mlflow decorators

--- src/cs_ubb_mlops_test/mlflow/wrappers/_base.py
+++ src/cs_ubb_mlops_test/mlflow/wrappers/_base.py
@@ -2,22 +2,36 @@ import functools
 from abc import ABCMeta, abstractmethod
 from typing import Callable, Any, Optional
 
-from mlflow.client import MlflowClient
+import mlflow
 from mlflow.models import infer_signature, ModelSignature
 
 
 class mlflow_decorator(metaclass=ABCMeta):
+    def __new__(cls, *args, **kwargs):
+        cls.__IMPORT_SUCCESS = True
+        try:
+            cls._unsafe_optional_import()
+        except ImportError:
+            cls.__IMPORT_SUCCESS = False
+        return super().__new__(cls)
+
     def __init__(self, enabled: bool, tracking_uri: str, experiment: Optional[str] = None) -> None:
-        self._mlflow = MlflowClient(tracking_uri if enabled else None)
+        if enabled:
+            mlflow.set_tracking_uri(tracking_uri)
         self._experiment = experiment
 
     def __ensure_experiment_id(self):
         if not self._experiment:
             return None
-        exp = self._mlflow.get_experiment_by_name(self._experiment)
+        exp = mlflow.get_experiment_by_name(self._experiment)
         if exp:
             return exp.experiment_id
-        return self._mlflow.create_experiment(self._experiment)
+        return mlflow.create_experiment(self._experiment)
+
+    @classmethod
+    @abstractmethod
+    def _unsafe_optional_import(cls) -> None:
+        pass
 
     @abstractmethod
     def _log_model(self, model: Any, sig: ModelSignature) -> None:
@@ -28,40 +42,52 @@ class mlflow_decorator(metaclass=ABCMeta):
     def _model_path(self) -> str:
         return ""
 
+    def _extra_log_model_args(self, sig: ModelSignature) -> dict:
+        result = {"signature": sig}
+        if self._experiment is not None:
+            result["registered_model_name"] = f"{self._experiment}-{self._model_path}"
+        return result
+
+    @staticmethod
+    def _process_wrapped_func_result(result: Any) -> tuple:
+        if result is None:
+            raise TypeError("model training function returned NoneType")
+        if not isinstance(result, tuple):
+            raise TypeError("model training function did not return tuple")
+        if len(result) != 3:
+            raise ValueError("expected training to return tuple with 3 values")
+        data_in, model_out, model = result
+        return data_in, model, model_out
+
     def __call__(self, wrapped: Callable[..., ModelSignature]) -> Callable:
         @functools.wraps(wrapped)
         def wrapper(*args, **kwargs) -> Any:
-            run_args = {}
-            experiment_id = self.__ensure_experiment_id()
-            if experiment_id:
-                run_args["experiment_id"] = experiment_id
-            run = self._mlflow.create_run(**run_args)
             run_finish_status = "FINISHED"
             try:
+                run_args = {}
+                experiment_id = self.__ensure_experiment_id()
+                if experiment_id:
+                    run_args["experiment_id"] = experiment_id
+
+                mlflow.start_run(**run_args)
                 model_params = {
                     f"param_{idx+1}": arg
                     for idx, arg in enumerate(args)
                 }
                 model_params.update(**kwargs)
-                for param_name, param_value in model_params.items():
-                    self._mlflow.log_param(run.info.run_id, param_name, param_value)
+                mlflow.log_params(model_params)
+
                 result = wrapped(*args, **kwargs)
+                data_in, model, model_out = self._process_wrapped_func_result(result)
 
-                if result is None:
-                    raise TypeError("model training function returned NoneType")
-                if not isinstance(result, tuple):
-                    raise TypeError("model training function did not return tuple")
-                if len(result) != 3:
-                    raise ValueError("expected training to return tuple with 3 values")
-                data_in, model_out, model = result
-
-                self._log_model(model, infer_signature(data_in, model_out, model_params))
+                if self.__IMPORT_SUCCESS:
+                    self._log_model(model, infer_signature(data_in, model_out, model_params))
 
                 return result
             except Exception as exc:
                 run_finish_status = "FAILED"
                 raise exc
             finally:
-                self._mlflow.update_run(run.info.run_id, run_finish_status)
+                mlflow.end_run(run_finish_status)
 
         return wrapper
