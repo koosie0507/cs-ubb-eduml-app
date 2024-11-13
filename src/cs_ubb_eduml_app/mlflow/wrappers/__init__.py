b5f6e714827f7d0336acb98cd4fd0cdde6be7fb1
Andrei Olar <andrei.olar@gmail.com>
2024-11-13 17:32:03 +0200

    add tensorflow & keras decorators

--- src/cs_ubb_eduml_app/mlflow/wrappers/__init__.py
+++ src/cs_ubb_eduml_app/mlflow/wrappers/__init__.py
@@ -1,5 +1,6 @@
 from cs_ubb_eduml_app.mlflow.wrappers._sklearn import sklearn_model
+from cs_ubb_eduml_app.mlflow.wrappers._tf import keras_model, tf_model
 from cs_ubb_eduml_app.mlflow.wrappers._torch import torch_model
 
 
-__all__ = ["sklearn_model", "torch_model"]
+__all__ = ["keras_model", "sklearn_model", "tf_model", "torch_model"]
